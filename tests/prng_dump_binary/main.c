c57986441948f8f1e7fd36ec5a75f56ade95a628
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2018-12-13 09:48:38 +0100

    add hwrng capability

--- tests/prng_dump_binary/main.c
+++ tests/prng_dump_binary/main.c
@@ -21,12 +21,26 @@
 #include <stdio.h>
 #include "random.h"
 
+#ifdef MODULE_PERIPH_HWRNG
+#include "periph/hwrng.h"
+#endif
+
+
 int main(void)
 {
+#ifdef MODULE_PERIPH_HWRNG
+    hwrng_init();
+#else
     random_init(534571505);
+#endif
 
     while(1) {
-        uint32_t rand_val = random_uint32();
+        uint32_t rand_val=0;
+#ifdef MODULE_PERIPH_HWRNG
+        hwrng_read(&rand_val, 4);
+#else
+        rand_val = random_uint32();
+#endif
         for (unsigned i =0; i< sizeof(uint32_t); i++) {
             printf("%c", (char)( (rand_val >> (8*i) & 0xff)) );
         }
