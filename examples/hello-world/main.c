d756fd1034c5dddb2282a4db4708268b10e14f33
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2021-03-22 13:31:20 +0100

    bliblablu

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,13 +20,41 @@
  */
 
 #include <stdio.h>
+#include "crypto/aes.h"
+
+static uint8_t TEST_0_KEY[] = {
+    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF
+};
+
+static uint8_t TEST_0_INP[] = {
+    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+};
+
+static uint8_t TEST_0_ENC[] = {
+    0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
+    0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6
+};
 
 int main(void)
 {
-    puts("Hello World!");
+    cipher_context_t ctx;
+    int err;
+    uint8_t data[16];
 
-    printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
-    printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-
+    err = aes_init(&ctx, TEST_0_KEY, sizeof(TEST_0_KEY));
+    if (err <= 0) {
+        puts("aes_init err <= 0");
+    }
+    err = aes_encrypt(&ctx, TEST_0_INP, data);
+    if (err <= 0) {
+        puts("aes_init err <= 0");
+    }
+    err = aes_decrypt(&ctx, TEST_0_ENC, data);
+    if (err <= 0) {
+        puts("aes_init err <= 0");
+    }
+    puts("OK");
     return 0;
 }
